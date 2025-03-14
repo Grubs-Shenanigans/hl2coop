31bae706e367da0d14f00402c82b50e79f281651
Bitl <bitlblox@gmail.com>
2025-03-14 13:45:24 -0700

    add reset.bat + add mod dirs

---
+++ game/greymod/reset.bat
@@ -0,0 +1,68 @@
+@ECHO OFF
+echo Resetting game...
+del voice_ban.dt
+del serverconfig.vdf
+del stats.txt
+del firefightreloaded_pak.vpk.sound.cache
+del firefightreloaded_sound.vpk.sound.cache
+del modelsounds.cache
+del hl2mp_shared.vpk.sound.cache
+del demoheader.tmp
+del textwindow_temp.html
+del console.log
+del singleplayerconfig.vdf
+del stats.txt
+del gamestate.txt
+del videoconfig_linux.cfg
+del glshaders.cfg
+del ep1_gamestats.dat
+del tf2_playerstats.dmx
+del cfg\config.cfg
+del cfg\settings.scr
+del cfg\user.scr
+del cfg\steam_autocloud.vdf
+del cfg\local_loadout.txt
+del bin\client.pdb
+del bin\gamepadui.pdb
+del bin\server.pdb
+del bin\client.so.dbg
+del bin\client_srv.so.dbg
+del bin\server.so.dbg
+del bin\server_srv.so.dbg
+del bin\gamepadui.so.dbg
+del bin\gamepadui_srv.so.dbg
+del bin_linux\client.so.dbg
+del bin_linux\client_srv.so.dbg
+del bin_linux\server.so.dbg
+del bin_linux\server_srv.so.dbg
+del bin_linux\gamepadui.so.dbg
+del bin_linux\gamepadui_srv.so.dbg
+del bin\x64\client.pdb
+del bin\x64\gamepadui.pdb
+del bin\x64\server.pdb
+del bin\x64\client.so.dbg
+del bin\x64\client_srv.so.dbg
+del bin\x64\server.so.dbg
+del bin\x64\server_srv.so.dbg
+del binx64\\gamepadui.so.dbg
+del bin\x64\gamepadui_srv.so.dbg
+del bin_linux\x64\client.so.dbg
+del bin_linux\x64\client_srv.so.dbg
+del bin_linux\x64\server.so.dbg
+del bin_linux\x64\server_srv.so.dbg
+del bin_linux\x64\gamepadui.so.dbg
+del bin_linux\x64\gamepadui_srv.so.dbg
+del sound\sound.cache
+rmdir /S /Q materials\vgui\logos\ui\
+rmdir /S /Q materials\
+rmdir /S /Q models\
+rmdir /S /Q sound\
+rmdir /S /Q sound_workshop\
+rmdir /S /Q download\user_custom\
+rmdir /S /Q download\sound\
+rmdir /S /Q download\materials\
+rmdir /S /Q download\models\
+rmdir /S /Q downloadlists\
+rmdir /S /Q save\
+rmdir /S /Q media\
+rmdir /S /Q replay\
