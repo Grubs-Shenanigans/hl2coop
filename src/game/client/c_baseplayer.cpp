8e2ed37c6e12f5cb4a12e93eecb06b4300380ecb
Bitl <bitlblox@gmail.com>
2025-07-05 20:19:37 -0700

    Implement #1392

--- src/game/client/c_baseplayer.cpp
+++ src/game/client/c_baseplayer.cpp
@@ -430,6 +430,11 @@ LINK_ENTITY_TO_CLASS( player, C_BasePlayer );
 C_BasePlayer::C_BasePlayer() : m_iv_vecViewOffset( "C_BasePlayer::m_iv_vecViewOffset" )
 {
 	AddVar( &m_vecViewOffset, &m_iv_vecViewOffset, LATCH_SIMULATION_VAR );
+
+#ifdef BDSBASE
+	AddVar(&m_Local.m_vecPunchAngle, &m_Local.m_iv_vecPunchAngle, LATCH_SIMULATION_VAR);
+	AddVar(&m_Local.m_vecPunchAngleVel, &m_Local.m_iv_vecPunchAngleVel, LATCH_SIMULATION_VAR);
+#endif
 	
 #ifdef _DEBUG																
 	m_vecLadderNormal.Init();
