c7d99d615b7bb1bc003cbf8f12329b26bfa79939
Bitl <bitlblox@gmail.com>
2025-07-10 12:57:12 -0700

    Implement #1403

--- src/game/client/tf/tf_hud_target_id.cpp
+++ src/game/client/tf/tf_hud_target_id.cpp
@@ -621,12 +621,20 @@ void CTargetID::PerformLayout( void )
 		iWidth += m_pAvatarImage->GetWide() + XRES( 2 );
 	}
 
+#ifdef BDSBASE
+	int iTextW;
+#else
 	int iTextW, iTextH;
+#endif
 	int iDataW, iDataH;
 
 	if ( m_pTargetNameLabel && m_pTargetDataLabel )
 	{
-		m_pTargetNameLabel->GetContentSize( iTextW, iTextH );
+#ifdef BDSBASE
+		m_pTargetNameLabel->GetTextImage()->GetDrawWidth(iTextW);
+#else
+		m_pTargetNameLabel->GetContentSize(iTextW, iTextH);
+#endif
 		m_pTargetDataLabel->GetContentSize( iDataW, iDataH );
 		iWidth += MAX(iTextW,iDataW);
 
