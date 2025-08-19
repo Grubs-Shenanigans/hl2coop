0428a4658057490422bc83db4010562034656349
Bitl <bitlblox@gmail.com>
2025-08-18 21:44:59 -0700

    add base TF admin command module (no commands yet)

---
+++ src/game/server/tf/admin/command/serveradmin_command_tf.h
@@ -0,0 +1,19 @@
+#ifndef SERVERADMIN_COMMAND_TF_H
+#define SERVERADMIN_COMMAND_TF_H
+
+#include "admin\base_serveradmin.h"
+#include "filesystem.h"
+#include "tf_player.h"
+
+#ifdef BDSBASE
+
+#define COMMAND_MODULE_NAME "Team Fortress 2 Commands"
+
+static void LoadTFCommandModule()
+{
+	// TODO: add commands
+}
+
+#endif
+
+#endif // SERVERADMIN_COMMAND_TF_H
