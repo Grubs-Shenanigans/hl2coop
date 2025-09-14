62413b7b51d5e6af7b3e658a23847a813737d5db
Bitl <bitlblox@gmail.com>
2025-09-14 10:21:34 -0700

    Implement #1528

--- src/game/shared/tf/tf_weapon_sniperrifle.cpp
+++ src/game/shared/tf/tf_weapon_sniperrifle.cpp
@@ -481,7 +481,13 @@ void CTFSniperRifle::PlayWeaponShootSound( void )
 	{
 		float flDamageBonus = 1.0f;
 		CALL_ATTRIB_HOOK_FLOAT( flDamageBonus, sniper_full_charge_damage_bonus );
+#ifdef BDSBASE
+		// Prevents Non-Machina rifles with "sniper_full_charge_damage_bonus" 
+		// from playing "SPECIAL3", which usually is Undefined
+		if (flDamageBonus > 1.0f && GetRifleType() == RIFLE_MACHINA)
+#else
 		if ( flDamageBonus > 1.0f )
+#endif
 		{
 			WeaponSound( SPECIAL3 );
 			return;
